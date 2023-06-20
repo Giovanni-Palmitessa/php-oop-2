93d0b9bbc6468c024bda629afbd8b72f7d0e9dca
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-06-20 16:49:25 +0200

    add first product (crocchette)

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+
+$products = [
+    new Product(
+        "Cane",
+        "Crocchette",
+        "Le crocchette di Monge Salmone e Riso All Breeds Adult sono un alimento completo per cani adulti di tutte le taglie formulato con un'unica fonte proteica: il salmone.",
+        "€48,90",
+        "assets/img/crocchette1.jpg",
+        "Salmone",
+        300,
+        5,
+    ),
+];
