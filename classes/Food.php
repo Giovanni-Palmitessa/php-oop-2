00502d74be3ecdd729cba855fd5d705a1c53ed65
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-06-20 16:16:50 +0200

    add get and set for ingredients

--- classes/Food.php
+++ classes/Food.php
@@ -5,4 +5,16 @@ class Food extends Product
     private string $ingredients;
     private string $calories;
     private int $weight;
+
+    public function setIngredients($ingredients)
+    {
+        if (strlen($ingredients) < 5) {
+            die('Nome troppo corto');
+        }
+        $this->ingredients = $ingredients;
+    }
+    public function getName()
+    {
+        return $this->ingredients;
+    }
 }
