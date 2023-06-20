5ea7821513a663951f1ff7a698cbfe9247f9b155
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-06-20 16:02:46 +0200

    add constructor in class product

--- classes/Product.php
+++ classes/Product.php
@@ -1,8 +1,16 @@
 <?php
+// instance variables
 class Product
 {
     public string $animal;
     public string $title;
     public string $description;
     public string $price;
+    public string $urlImage;
+
+    // constuctor
+
+    public function __construct(string $animal, string $title, string $description, string $price, string $urlImage)
+    {
+    }
 }
