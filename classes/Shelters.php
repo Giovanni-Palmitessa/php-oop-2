b4d58b70ea1586c6c5d74527f091b78b1c1e7290
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-06-20 18:11:18 +0200

    add constructor to shelters

--- classes/Shelters.php
+++ classes/Shelters.php
@@ -5,6 +5,21 @@ class Shelters extends Product
     private string $size;
     private string $shape;
 
+    // constructor
+    public function __construct(
+        string $category,
+        string $title,
+        string $description,
+        string $price,
+        string $urlImage,
+        string $size,
+        string $shape
+    ) {
+        parent::__construct($category, $title, $description, $price, $urlImage);
+        $this->size = $size;
+        $this->shape = $shape;
+    }
+
     // GET AND SET FUNCTIONS
     // size
 
