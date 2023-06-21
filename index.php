d5f0e60772b858994bb247c260186214248f4cdd
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-06-21 14:58:07 +0200

    import fa cdn

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ include __DIR__ . '/db.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Php OOP 2</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="assets/style.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous" defer></script>
 </head>
@@ -48,6 +49,8 @@ include __DIR__ . '/db.php';
                         <!-- SHELTER -->
                         <?php if ($product instanceof Shelter) { ?>
                             <li class="list-group-item">Taglia: <?= $product->getSize() ?></li><?php } ?>
+                        <?php if ($product instanceof Shelter) { ?>
+                            <li class="list-group-item">Forma: <?= $product->getShape() ?></li><?php } ?>
                     </ul>
                     <div class="card-body">
                         <a href="#" class="card-link">Aggiungi al carrello</a>
